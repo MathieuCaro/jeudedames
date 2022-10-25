29f8cf00ecd9b41b7201784ac8a929e7270a1c21
MathieuC98 <mathieucaro123@outlook.com>
2022-10-25 16:47:24 +0200

    ajout carrés blancs

--- jeudedames.c
+++ jeudedames.c
@@ -3,19 +3,27 @@
 
 #include "jeudedames.h"
 
+#define RED   "\x1B[31m"
+#define RESET "\x1B[0m"
+#define CYN   "\x1B[36m"
+#define YEL   "\x1B[33m"
+#define FONDBC   "\x1B[47m"
+#define GRAS   "\x1B[1m"
+
+
 void fill_grid(char tab[][SIZE_GRID]){
     for(int i=0;i<SIZE_GRID;i++){
         for(int j=0; j<SIZE_GRID; j++){
             if ((i+j)%2==0){
-                tab[i][j]=' '; // ='X';
+                tab[i][j]=' ';
             }
             else if(((i+j)%2!=0)&&(i<=3)){ 
                 tab[i][j]='B';
             }
             else if(((i+j)%2!=0)&&(i>=3)&&(i<=5)){ 
                 tab[i][j]=' ';
+                
             }
-
             else if(((i+j)%2!=0)&&(i>=5)){ 
                 tab[i][j]='W';
             }
@@ -23,23 +31,32 @@ void fill_grid(char tab[][SIZE_GRID]){
     }
 }
 
+
 void show_board(char board[][SIZE_GRID]){
     int i,j;
     printf("  |");
     for (i=0;i<SIZE_GRID;i++){
-        printf("%d |", i);
+        printf(YEL " %d" RESET, i);
+        printf("|");
     }
     printf("\n");
     for (i=0;i<SIZE_GRID;i++){
-        printf("%d |", i);
+        printf(YEL " %d" RESET, i);
+        printf("|");
         for(j=0;j<SIZE_GRID;j++){
             if (board[i][j] == 'B'){
-                printf("B |");
+                printf(GRAS RED "B " RESET);
+                printf("|");
             }
             if (board[i][j] == 'W'){
-                printf("W |");
+                printf(GRAS CYN "W " RESET);
+                printf("|");
             }
-            if (board[i][j] == 'X' || board[i][j] == ' '){
+            if (board[i][j] == ' ' && (i+j)%2==0){
+                printf(FONDBC"  "RESET);
+                printf("|");
+            }
+            if (board[i][j] == ' ' && (i+j)%2!=0){
                 printf("  |");
             }
         }
@@ -48,18 +65,6 @@ void show_board(char board[][SIZE_GRID]){
     printf("\n");
 }
 
-void display_grid(char tab[][SIZE_GRID]){
-    int i,j;
-    for(i=0; i<SIZE_GRID; i++){
-        for(j=0; j<SIZE_GRID; j++){
-            printf(" %c",tab[i][j]);
-        }
-        printf("\n");
-        
-        
-    }
-    printf("\n");
-}
 
 int is_void_case(char cas){
     if (cas ==' '){
@@ -70,6 +75,7 @@ int is_void_case(char cas){
     }
 }
 
+
 int is_black_case(int pos_final[]){
     if ((pos_final[0]+pos_final[1]) %2 == 1){
         return 1;
@@ -78,6 +84,7 @@ int is_black_case(int pos_final[]){
         return 0;
     }
 }
+
 
 int is_pawn(char cas){
     if ((cas == 'B' ) || (cas == 'W'))
@@ -88,6 +95,7 @@ int is_pawn(char cas){
         return 0;
     } 
 }
+
 
 int is_possible_move(char tab[][SIZE_GRID],int pos_init[], int pos_final[]){
     int nb_move_x = abs(pos_init[0]-pos_final[0]);
@@ -109,25 +117,24 @@ int is_possible_move(char tab[][SIZE_GRID],int pos_init[], int pos_final[]){
     else
         printf("Rentrez un coup valide ! (Deplacer un pion d'une seule case)\n");
         return 0;
-
-
 }
 
 
-int diagonal_pawns(char tab[][SIZE_GRID], int pos_init[], char color, int eatable_pawn[][4], int size_eatable_pawn){
-    
-    
+void diagonal_pawns(char tab[][SIZE_GRID], int pos_init[], char color, int eatable_pawn[][6], int *size_eatable_pawn){
+      
     if(pos_init[1]+2 < SIZE_GRID && pos_init[1]+2 >= 0 
         && pos_init[0]+2 < SIZE_GRID && pos_init[0]+2 >= 0
         && tab[pos_init[1]+1][pos_init[0]+1]!=color
         && tab[pos_init[1]+1][pos_init[0]+1] != ' '){
 
             if(tab[pos_init[1]+2][pos_init[0]+2]==' '){
-                eatable_pawn[size_eatable_pawn][0] = pos_init[0]+1;
-                eatable_pawn[size_eatable_pawn][1] = pos_init[1]+1;
-                eatable_pawn[size_eatable_pawn][2] = pos_init[0]+2;
-                eatable_pawn[size_eatable_pawn][3] = pos_init[1]+2;
-                size_eatable_pawn++;
+                eatable_pawn[*size_eatable_pawn][0] = pos_init[0];
+                eatable_pawn[*size_eatable_pawn][1] = pos_init[1];
+                eatable_pawn[*size_eatable_pawn][2] = pos_init[0]+1;
+                eatable_pawn[*size_eatable_pawn][3] = pos_init[1]+1;
+                eatable_pawn[*size_eatable_pawn][4] = pos_init[0]+2;
+                eatable_pawn[*size_eatable_pawn][5] = pos_init[1]+2;
+                *size_eatable_pawn += 1;
             }
         }
 
@@ -137,14 +144,15 @@ int diagonal_pawns(char tab[][SIZE_GRID], int pos_init[], char color, int eatabl
         && tab[pos_init[1]+1][pos_init[0]-1]!=' '){
 
             if(tab[pos_init[1]+2][pos_init[0]-2]==' '){
-                eatable_pawn[size_eatable_pawn][0] = pos_init[0]-1;
-                eatable_pawn[size_eatable_pawn][1] = pos_init[1]+1;
-                eatable_pawn[size_eatable_pawn][2] = pos_init[0]-2;
-                eatable_pawn[size_eatable_pawn][3] = pos_init[1]+2;
-                size_eatable_pawn++;
+                eatable_pawn[*size_eatable_pawn][0] = pos_init[0];
+                eatable_pawn[*size_eatable_pawn][1] = pos_init[1];
+                eatable_pawn[*size_eatable_pawn][2] = pos_init[0]-1;
+                eatable_pawn[*size_eatable_pawn][3] = pos_init[1]+1;
+                eatable_pawn[*size_eatable_pawn][4] = pos_init[0]-2;
+                eatable_pawn[*size_eatable_pawn][5] = pos_init[1]+2;
+                *size_eatable_pawn += 1;
             }
         }
-
     
     if(pos_init[1]-2 < SIZE_GRID && pos_init[1]-2 >= 0
         && pos_init[0]+2 < SIZE_GRID && pos_init[0]+2 >= 0
@@ -152,52 +160,69 @@ int diagonal_pawns(char tab[][SIZE_GRID], int pos_init[], char color, int eatabl
         && tab[pos_init[1]-1][pos_init[0]+1]!=' '){
 
             if(tab[pos_init[1]-2][pos_init[0]+2]==' '){
-                eatable_pawn[size_eatable_pawn][0] = pos_init[0]+1;
-                eatable_pawn[size_eatable_pawn][1] = pos_init[1]-1;
-                eatable_pawn[size_eatable_pawn][2] = pos_init[0]+2;
-                eatable_pawn[size_eatable_pawn][3] = pos_init[1]-2;
-                size_eatable_pawn++;
+                eatable_pawn[*size_eatable_pawn][0] = pos_init[0];
+                eatable_pawn[*size_eatable_pawn][1] = pos_init[1];
+                eatable_pawn[*size_eatable_pawn][2] = pos_init[0]+1;
+                eatable_pawn[*size_eatable_pawn][3] = pos_init[1]-1;
+                eatable_pawn[*size_eatable_pawn][4] = pos_init[0]+2;
+                eatable_pawn[*size_eatable_pawn][5] = pos_init[1]-2;
+                *size_eatable_pawn += 1;
             }
         }
+
     if(pos_init[1]-2 < SIZE_GRID && pos_init[1]-2 >= 0
         && pos_init[0]-2 < SIZE_GRID && pos_init[0]-2 >= 0
         && tab[pos_init[1]-1][pos_init[0]-1]!=color
         && tab[pos_init[1]-1][pos_init[0]-1]!=' '){
 
             if(tab[pos_init[1]-2][pos_init[0]-2]==' '){
-                eatable_pawn[size_eatable_pawn][0] = pos_init[0]-1;
-                eatable_pawn[size_eatable_pawn][1] = pos_init[1]-1;
-                eatable_pawn[size_eatable_pawn][2] = pos_init[0]-2;
-                eatable_pawn[size_eatable_pawn][3] = pos_init[1]-2;
-                size_eatable_pawn++;
+                eatable_pawn[*size_eatable_pawn][0] = pos_init[0];
+                eatable_pawn[*size_eatable_pawn][1] = pos_init[1];
+                eatable_pawn[*size_eatable_pawn][2] = pos_init[0]-1;
+                eatable_pawn[*size_eatable_pawn][3] = pos_init[1]-1;
+                eatable_pawn[*size_eatable_pawn][4] = pos_init[0]-2;
+                eatable_pawn[*size_eatable_pawn][5] = pos_init[1]-2;
+                *size_eatable_pawn += 1;
             }
-        }
-    
-    return size_eatable_pawn;
+        } 
 }
 
-int take_diagonal_pawns(char tab[][SIZE_GRID], int pos_init[], char color){
-    int eatable_pawn[20][4]; // position du pion mangeable puis position voulu pour manger le pion
+
+int pawn_suggested(char tab[][SIZE_GRID], char color, int eatable_pawn[][6]){
+
+    int index[2];
     int size_eatable_pawn = 0;
 
-    size_eatable_pawn = diagonal_pawns(tab, pos_init, color, eatable_pawn, size_eatable_pawn);
-
-    if (size_eatable_pawn != 0) {
-        printf("Le pion %c a la position (%d,%d) peut manger le pion en position : \n", color, pos_init[0], pos_init[1]);
-        for(int i = 0; i<size_eatable_pawn; i++){
-            printf(" - (%d,%d) deplacer votre pion a la case (%d,%d)\n", eatable_pawn[i][0],eatable_pawn[i][1],eatable_pawn[i][2],eatable_pawn[i][3]);
+    for (int x = 0; x < SIZE_GRID; x++){
+        for(int y = 0; y < SIZE_GRID; y++){
+            if (tab[y][x] == color){
+                index[0] = x;
+                index[1] = y;
+                
+                diagonal_pawns(tab, index, color, eatable_pawn, &size_eatable_pawn);
+            }
         }
     }
-    return size_eatable_pawn;
+    
+    if (size_eatable_pawn > 0) {
+        for(int i = 0; i < size_eatable_pawn; i++){
+            printf("Le pion %c a la position (%d,%d) peut se déplacer à la position (%d,%d) pour manger le pion (%d,%d) \n", color,\
+            eatable_pawn[i][0], eatable_pawn[i][1], eatable_pawn[i][4], eatable_pawn[i][5], eatable_pawn[i][2], eatable_pawn[i][3]);
+        }
+    }
+
+    if (size_eatable_pawn > 0){
+        return 1;
+    }
+    return 0;
 }
+
 
 void take_opponent_pawn(char tab[][SIZE_GRID], int pos_init[], int pos_final[], char color){
-    int index_x = (pos_init[0]+pos_final[0])/2;
-    int index_y = (pos_init[1]+pos_final[1])/2;
+    int index_x = (pos_init[0] + pos_final[0]) / 2;
+    int index_y = (pos_init[1] + pos_final[1]) / 2;
     tab[index_y][index_x] = ' ';    
 }
-
-
 
 int move_pawn(char tab[][SIZE_GRID],int pos_init[], int pos_final[], char color){
     int initial_case = tab[pos_init[1]][pos_init[0]];
@@ -206,56 +231,32 @@ int move_pawn(char tab[][SIZE_GRID],int pos_init[], int pos_final[], char color)
     if ((is_void_case(final_case)) && (is_pawn(initial_case)) && (is_black_case(pos_final)) && (is_possible_move(tab, pos_init, pos_final))){
         tab[pos_final[1]][pos_final[0]] = initial_case;
         tab[pos_init[1]][pos_init[0]] = ' ';
-        if (abs(pos_final[1]-pos_init[1]) == 2)
+        if (abs(pos_final[1] - pos_init[1]) == 2)
             take_opponent_pawn(tab, pos_init, pos_final, color); 
         return 1;
     }
     else{
         return 0;
     }
-       
 }
-
-int pawn_suggested(char tab[][SIZE_GRID], char color){
-
-    int index[2];
-    int possible_hit = 0;
-
-    for (int x=0; x<SIZE_GRID; x++){
-        for(int y=0; y<SIZE_GRID; y++){
-            if (tab[y][x]==color){
-                index[0] = x;
-                index[1] = y;
-                possible_hit = take_diagonal_pawns(tab, index, color);
-            }
-        }
-    }
-    if (possible_hit>0){
-        return 1;
-    }
-    return 0;
-}
-
-
 
 
 int is_end_game(char tab[][SIZE_GRID]){
     int black_pawn = 0;
     int white_pawn = 0;
 
-    for (int x=0; x<SIZE_GRID; x++){
-        for(int y=0; y<SIZE_GRID; y++){
-            if (tab[y][x]=='B'){
+    for (int x = 0; x < SIZE_GRID; x++){
+        for(int y = 0; y < SIZE_GRID; y++){
+            if (tab[y][x] == 'B'){
                 black_pawn = 1;
             }
-            else if (tab[y][x]=='W'){
+            else if (tab[y][x] == 'W'){
                 white_pawn = 1;
             }
 
-            if (black_pawn+white_pawn==2){
+            if (black_pawn + white_pawn == 2){
                 return 0;
             }
-    
         }
     }
     if (black_pawn == 0){
--- main.c
+++ main.c
@@ -3,55 +3,74 @@
 
 #include "jeudedames.h"
 
+#define RED   "\x1B[31m"
+#define RESET "\x1B[0m"
+#define CYN   "\x1B[36m"
+
+
+void select_pawns(int pos_init[2], int pos_final[2], char color){
+    if (color == 'W'){
+        printf(CYN "Joueur blanc : Quel pion voulez-vous deplacer ? " RESET);
+        scanf("%d %d", &pos_init[0], &pos_init[1]);
+                    
+        printf(CYN "Joueur blanc : Ou voulez vous le deplacer ? " RESET);
+        scanf("%d %d", &pos_final[0], &pos_final[1]);
+    }
+    else {
+        printf(RED "Joueur noir : Quel pion voulez-vous deplacer ? " RESET);
+        scanf("%d %d", &pos_init[0], &pos_init[1]);
+                    
+        printf(RED "Joueur noir : Ou voulez vous le deplacer ? " RESET);
+        scanf("%d %d", &pos_final[0], &pos_final[1]);
+    }
+}
 
 void game(char tab[][SIZE_GRID]){
     int pos_init[2];
     int pos_final[2];
 
-    int is_possible_move = 0;
-    int is_rafle = 1;
+    int is_possible_move = 0; //FALSE
+    int is_rafle = 1; // TRUE
+    int eatable_pawn[20][6]; // Position du pion à déplacer / Position du pion mangeable / Position final pour le manger
 
-    int player = 1;
+    char player = 'W';
 
     fill_grid(tab);
     show_board(tab);
 
     while(is_end_game(tab) == 0){
-        if (player == 1){
-            is_possible_move = 0;
-            is_rafle = 1;
-            while (is_possible_move == 0 || is_rafle == 1){
-                is_rafle = pawn_suggested(tab,'W');
-                printf("%d", is_rafle);
-                printf("Joueur blanc : Quel pion voulez-vous deplacer ? ");
-                scanf("%d %d", &pos_init[0], &pos_init[1]);
+        is_possible_move = 0;
+        is_rafle = pawn_suggested(tab, player, eatable_pawn);
+
+        if (is_rafle == 0){// Pas de rafle possible
+            while (is_possible_move == 0){
+                        select_pawns(pos_init, pos_final, player);
+                        is_possible_move = move_pawn(tab,pos_init, pos_final, player);
+                    }
+        }
+        else{ // Rafle possible
+            while (is_possible_move == 0 || is_rafle == 1){ // Si la rafle est possible
                 
-                printf("Joueur blanc : Ou voulez vous le deplacer ? ");
-                scanf("%d %d", &pos_final[0], &pos_final[1]);
-                is_possible_move = move_pawn(tab,pos_init, pos_final,'W');
+                select_pawns(pos_init, pos_final, player);
+        
+                for(int i=0; i<20; i++){
+                    
+                    if (pos_final[0] == eatable_pawn[i][4] && pos_final[1] == eatable_pawn[i][5]){
+                        is_possible_move = move_pawn(tab,pos_init, pos_final, player);
+                        break;
+                    }       
+                }                   
+                is_rafle = pawn_suggested(tab, player, eatable_pawn);
             }
-            player = 2;
-        }
-        else{
-           is_possible_move = 0;
-           is_rafle = 1;
-            while (is_possible_move == 0 || is_rafle == 1){
-                is_rafle = pawn_suggested(tab,'B');
-                printf("Joueur noir : Quel pion voulez-vous deplacer ? ");
-                scanf("%d %d", &pos_init[0], &pos_init[1]);
-                  
-                printf("Joueur noir : Ou voulez vous le deplacer ? ");
-                scanf("%d %d", &pos_final[0], &pos_final[1]);
-                is_possible_move = move_pawn(tab,pos_init, pos_final,'B');
-            }
-            player = 1;
-        }
+        }      
+
+        if (player == 'W')
+            player = 'B';
+        else 
+            player = 'W';
         
         show_board(tab);
-
     }
-
-
 }
 
 int main(void){
